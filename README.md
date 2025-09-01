b3d604d71f83077cfae3d9cdc25cf6584172ef38
Alex. <alex.liska04@seznam.cz>
2025-09-02 00:36:15 +0200

    feat: Add a polished layout       Finish a color palette refactor: Restructure CSS

--- README.md
+++ README.md
@@ -2,21 +2,38 @@
 
 Personal project focusing on classes and heavy javascript interactions.
 
-## Description
-
-A basic to-do list where the user can add, remove and filter tasks. 
-
-Task inputs include name, description, due date etc. 
-
+## Features
+- Add, remove, and filter tasks
+- Different task types: check/daily, long-term/status, notes
+- Color-coded UI (custom palette)
 
 ## Version
-- v1.0 - 31.08.2025
-  Initial commit - basic layout / functions.
+**v2.0 - 02.09.2025**
+- Complete redesign of layout and CSS
+- Added custom color palette
 
-## TODO
- [ ] Finish basic layout.
- [X] Add remove task feature.
- [ ] ID changes.
- [ ] Add proper filtering.
- [ ] Add more task input parameters.
- [ ] Make a proper color palette.
+ **v1.0 - 31.08.2025**
+- Initial commit - basic layout / functions
+
+## Built With
+- HTML
+- CSS
+- JavaScript 
+
+## Planned Features
+- [X] Finish basic layout.
+- [X] Add task remove feature.
+- [X] Add different types of tasks / notes.
+- [X] Make a proper color palette.
+- [ ] Add localStorage.
+- [ ] Add status type task add function.
+- [ ] Add note add function.
+- [ ] ID changes.
+- [ ] Add proper filtering.
+- [ ] Add more task input parameters.
+
+
+## Installation
+Clone the repo:
+```bash
+git clone https://github.com/Alex-0234/todo-list-v1.git
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let dailyTasks = [];
+let statusTasks = [];
+let notes = [];
 
 let primaryColor = null;
 let secondaryColor = null;
@@ -7,45 +10,115 @@ let textColor = null;
 document.addEventListener('DOMContentLoaded', ()=> {
   console.log('Inicialized');
 
-
-  primaryColor = getComputedStyle(document.body).getPropertyValue("--primary-color");
-  secondaryColor = getComputedStyle(document.body).getPropertyValue("--secondary-color");
   bgColor = getComputedStyle(document.body).getPropertyValue("--bg-color");
   textColor = getComputedStyle(document.body).getPropertyValue("--text-color");
+  primaryColor = getComputedStyle(document.body).getPropertyValue("--primary-color");
+  secondaryColor = getComputedStyle(document.body).getPropertyValue("--secondary-color");
 
+  const nav1 = document.querySelector('#daily');
+  const nav2 = document.querySelector('#longterm');
+  const nav3 = document.querySelector('#notes');
 
-  const sectionButton = document.querySelector('.addButton');
+  const daily = document.querySelector('.checkList');
+  const statusBased = document.querySelector('.statusList');
+  const notes = document.querySelector('.notes');
 
-  sectionButton.addEventListener('click', ()=> {
-    const setup = new Setup();
+  const addButtonCheck = document.querySelector('.add-button-check');
+  const addButtonStatus = document.querySelector('.add-button-status');
+
+  addButtonCheck.addEventListener('click', ()=> {
+    if (!document.querySelector('.setup')) {
+        const setup = new Setup('check'); // Change needed. If I click on addButon and other button setup is there it causes confusion..
+    }
   })
+  addButtonStatus.addEventListener('click', ()=> {
+    if (!document.querySelector('.setup')) {
+        const setup = new Setup('status'); // Change needed. If I click on addButon and other button setup is there it causes confusion..
+    }
+  })
+  nav1.addEventListener('click', (e)=> {
+     e.preventDefault();
+
+     nav1.classList.add('underline');
+     nav2.classList.remove('underline');
+     nav3.classList.remove('underline');
+
+     daily.classList.remove('ofscreen-left');
+     daily.classList.remove('ofscreen-right');
+     daily.classList.add('onscreen-center');
+
+     statusBased.classList.remove('onscreen-center');
+     statusBased.classList.remove('ofscreen-left');
+     statusBased.classList.add('ofscreen-right');
+
+     notes.classList.remove('ofscreen-right');
+     notes.classList.remove('onscreen-center');
+     notes.classList.add('ofscreen-left');
+  });
+
+  nav2.addEventListener('click', (e)=> {
+     e.preventDefault();
+
+     nav1.classList.remove('underline');
+     nav2.classList.add('underline');
+     nav3.classList.remove('underline');
+
+     daily.classList.add('ofscreen-left');
+     daily.classList.remove('ofscreen-right');
+     daily.classList.remove('onscreen-center');
+
+     statusBased.classList.add('onscreen-center');
+     statusBased.classList.remove('ofscreen-left');
+     statusBased.classList.remove('ofscreen-right');
+
+     notes.classList.add('ofscreen-right');
+     notes.classList.remove('onscreen-center');
+     notes.classList.remove('ofscreen-left');
+  });
+
+  nav3.addEventListener('click', (e)=> {
+     e.preventDefault();
+
+     nav1.classList.remove('underline'); 
+     nav2.classList.remove('underline');
+     nav3.classList.add('underline');
+
+     daily.classList.remove('ofscreen-left');
+     daily.classList.add('ofscreen-right');
+     daily.classList.remove('onscreen-center');
+
+     statusBased.classList.remove('onscreen-center');
+     statusBased.classList.add('ofscreen-left');
+     statusBased.classList.remove('ofscreen-right');
+
+     notes.classList.remove('ofscreen-right');
+     notes.classList.add('onscreen-center');
+     notes.classList.remove('ofscreen-left');
+  });
 
 })
 class Setup {
-  constructor() {
-    this.container = document.querySelector('.addTask');
+  constructor(type) {
+    this.type = type;
+    this.container = document.querySelector('main');
+  
+    this.setupWindow = document.createElement('div');
+    this.setupWindow.classList.add('setup');
 
-    this.setup = document.createElement('div');
-    this.setup.classList.add('setup');
+      this.textareaWrapper = document.createElement('div');
+      this.textareaWrapper.classList.add('setup-textarea');
+      this.textareaLabel = document.createElement('label');
+      this.textareaLabel.innerHTML = "Describe your task:";
+      this.textarea = document.createElement('textarea');
+      this.textarea.classList.add('description');
 
-    this.firstRow = document.createElement('div');
-    this.firstRow.classList.add('setup-row1');
-
+    this.nameInputWrapper = document.createElement('div');
+    this.nameInputWrapper.classList.add('name-input-wrapper');
     this.nameLabel = document.createElement('label');
     this.nameLabel.innerHTML = "Name your task:";
-
     this.nameInput = document.createElement('input');
     this.nameInput.setAttribute('type', 'text');
     this.nameInput.classList.add('task-name');
-
-    this.textareaWrapper = document.createElement('div');
-    this.textareaWrapper.classList.add('setup-textarea');
-
-    this.textareaLabel = document.createElement('label');
-    this.textareaLabel.innerHTML = "Describe your task:";
-
-    this.textarea = document.createElement('textarea');
-    this.textarea.classList.add('description');
 
     this.addButtonArea = document.createElement('div');
     this.addButtonArea.classList.add('add-button-area');
@@ -61,61 +134,64 @@ class Setup {
     this.closeIcon.setAttribute('width', '30px');
     this.closeIcon.setAttribute('viewBox', '16 16 518 1020');
     
-
     this.path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
     this.path.setAttribute('d', 'M400 145.49L366.51 112L256 222.51L145.49 112L112 145.49L222.51 256L112 366.51L145.49 400L256 289.49L366.51 400L400 366.51L289.49 256L400 145.49z')
     this.path.setAttribute('fill', `${primaryColor}`);
 
-
     this.init();
     this.appendAll();
-
-    
   }
     init() {
-
       this.closeIcon.addEventListener('click', ()=> {
-        this.container.removeChild(this.setup);
+        this.container.removeChild(this.setupWindow);
       });
       this.button.addEventListener('click', ()=> {
-        const task = new Task(this.nameInput.value, this.textarea.value);
+        if (this.type === 'check') {
+          const task = new Task(this.nameInput.value, this.textarea.value, 'check');
+        } else {
+          const task = new Task(this.nameInput.value, this.textarea.value, 'status');
+        }
         this.nameInput.value = "";
         this.textarea.value = "";
       })
     }
     appendAll() {
-
-        this.firstRow.appendChild(this.nameLabel);
-        this.firstRow.appendChild(this.nameInput);
         this.textareaWrapper.appendChild(this.textareaLabel);
         this.textareaWrapper.appendChild(this.textarea);
+        this.nameInputWrapper.appendChild(this.nameLabel);
+        this.nameInputWrapper.appendChild(this.nameInput);
         this.addButtonArea.appendChild(this.button);
-        this.setup.appendChild(this.firstRow);
-        this.setup.appendChild(this.textareaWrapper);
-        this.setup.appendChild(this.addButtonArea);
+        this.setupWindow.appendChild(this.nameInputWrapper);
+        this.setupWindow.appendChild(this.textareaWrapper);
+        this.setupWindow.appendChild(this.addButtonArea);
         this.closeIcon.appendChild(this.path);
-        this.setup.appendChild(this.closeIcon); 
-        this.container.appendChild(this.setup);
+        this.setupWindow.appendChild(this.closeIcon); 
+        this.container.appendChild(this.setupWindow);  
     }
-  
 };
-
 
 class Task {
 
   static counter = 0;
 
-  constructor(name, description) {
+  constructor(name, description, type) {
 
-    this.taskList = document.querySelector('.taskList');
+    this.checkList = document.querySelector('.check-list');
+    this.statusList = document.querySelector('.status-list');
 
-    this.id = ++Task.counter;
+    this.id = Task.counter++; // Could add Date.now( ) id. 
     this.name = name;
     this.description = description;
+    this.type = type;
+
+    this.object = {
+      id: this.id,
+      name: this.name,
+      description: this.description,
+    }
 
     this.task = document.createElement('div');
-    this.task.classList.add('task-closed');
-
+    
     this.smallWrapper = document.createElement('div');
     this.smallWrapper.classList.add('small-wrapper');
     this.smallWrapper.classList.add('center');
@@ -124,19 +200,18 @@ class Task {
     this.openTaskButton.classList.add('task-open-button');
     this.openTaskButton.textContent = "See description";
 
+    this.nameOutput = document.createElement('input');
+    this.nameOutput.classList.add('name-output')
+    this.nameOutput.setAttribute('readonly', '');
+    this.nameOutput.value = `${name}`;
+
     this.checkbox = document.createElement('input');
     this.checkbox.type = 'checkbox';
     this.checkbox.classList.add('checkbox');
 
-    this.num = document.createElement('input');
-    this.num.setAttribute('readonly', '');
-    this.num.classList.add('num');
-    this.num.value = `${this.id}.`;
-
     this.textarea = document.createElement('textarea');
     this.textarea.classList.add('textarea');
     this.textarea.value = this.description;
-    this.textarea.classList.add('hidden');
 
     this.deleteIcon = document.createElementNS("http://www.w3.org/2000/svg",'svg');
     this.deleteIcon.classList.add('delete-icon');
@@ -146,32 +221,27 @@ class Task {
     this.deleteIcon.setAttribute('width', '20px');
     this.deleteIcon.innerHTML = `<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12l1.41 1.41L13.41 14l2.12 2.12l-1.41 1.41L12 15.41l-2.12 2.12l-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/>`; 
 
+   
 
-    this.smallWrapper.appendChild(this.checkbox);
-    this.smallWrapper.appendChild(this.openTaskButton);
-    this.smallWrapper.appendChild(this.num);
-    this.task.appendChild(this.smallWrapper);
-    this.task.appendChild(document.createTextNode(name));
-    this.task.appendChild(this.textarea);
-    this.task.appendChild(this.deleteIcon);
+
+    if (this.type === 'check') {
+      this.addCheckboxType();
+    } else if (this.type === 'status') {
+      this.addStatusType();
+    }
 
     this.init();
-    this.addTask(this.taskList);
   }
+   // ======================================================
+
   init() {
-    this.openTaskButton.addEventListener('click', ()=> {
-      this.task.classList.toggle('task-closed');
-      this.task.classList.toggle('task-open');
-      if (this.task.classList.contains('task-closed')) {
-        this.textarea.classList.add('hidden');
-      } else {
-        this.textarea.classList.remove('hidden');
-      }
-    })
+
     this.deleteIcon.addEventListener('click', ()=> {
        this.uSureOption(this.taskList);
     })
   }
+  // ======================================================
+
   uSureOption(parent) {
     
     this.window = document.createElement('div');
@@ -194,21 +264,47 @@ class Task {
 
     this.sure.addEventListener('click', ()=> {
       this.task.removeChild(this.window);
-      this.updateTaskNumbers();
       this.removeTask(parent);
       
     })
     this.unsure.addEventListener('click', ()=> {
-      this.task.removeChild(this.window);
+
+      this.task.removeChild(this.object);
     })
   }
+   // ======================================================
+   addCheckboxType() {
+      this.task.classList.add('task');
+      this.task.appendChild(this.checkbox);
+      this.task.appendChild(this.nameOutput);
+      this.task.appendChild(this.textarea);
+      this.task.appendChild(this.deleteIcon);
+      this.checkList.appendChild(this.task);
+
+      dailyTasks.push(this.object);
+   }
+   // ======================================================
+     addStatusType() {
+      
+
+
+   }
+   // ======================================================
   
-  addTask(parent) {
-    parent.appendChild(this.task);
+  addTask(type) {
+    if (type === 1) {
+      this.task.classList.add('checklist');
+      this.checkList.appendChild(this.task);
+    } else if (type === 2) {
+      this.statusList.appendChild(this.task);
+    }
+    
   }
+  // ======================================================
   removeTask(parent) {
     parent.removeChild(this.task);
   }
+   // ======================================================
 
   toggleCompleted() {
     this.task.classList.toggle('completed');
